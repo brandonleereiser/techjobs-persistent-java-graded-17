07bc26ba3628f96afeffb154666b854bea95fc28
Brandon Reiser <reiserb58@gmail.com>
2024-02-08 18:24:38 -0600

    Changes to complete assignment 4, 2/8/24

---
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -0,0 +1,44 @@
+package org.launchcode.techjobs.persistent.models;
+
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Job extends AbstractEntity {
+    @ManyToOne
+    private Employer employer;
+
+    @ManyToMany
+    private List<Skill> skills;
+
+
+    public Job() {
+    }
+
+    // Initialize the id and value fields.
+    public Job(Employer anEmployer, List someSkills) {
+        super();
+        this.employer = anEmployer;
+        this.skills = someSkills;
+    }
+
+    // Getters and setters.
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+}
